ddff47db12bcba35e080d55a13a97afe05226913
Chandan Kumar <Chandan_Kumar-Ggn@external.mckinsey.com>
2016-12-17 15:20:12 +0530

    code refactor

--- src/test/java/tdd/xtr/StringCalTest.java
+++ src/test/java/tdd/xtr/StringCalTest.java
@@ -1,21 +1,24 @@
 package tdd.xtr;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalTest {
+	StringCalculator strcalc;
 	
-	
+	@Before
+	public void setup(){
+		strcalc = new StringCalculator();
+	}
 	@Test
 	public void check_sum_for_empty_string(){
-		StringCalculator strcalc = new StringCalculator();
 		Assert.assertEquals(0,strcalc.checksum(""));
 	}
 	
 	@Test
 	public void check_sum_for_single_positive_no(){
-		StringCalculator strcalc = new StringCalculator();
-		Assert.assertEquals(1,strcalc.checksum("1"));
+		Assert.assertEquals(0,strcalc.checksum("0"));
 	}
 
 }
