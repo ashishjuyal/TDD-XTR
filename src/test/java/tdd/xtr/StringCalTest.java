faba9e473eb4574fbde451adb661b7f38dbd85b1
Chandan Kumar <Chandan_Kumar-Ggn@external.mckinsey.com>
2016-12-17 15:07:24 +0530

    failing the checksum method for empty string

---
+++ src/test/java/tdd/xtr/StringCalTest.java
@@ -0,0 +1,15 @@
+package tdd.xtr;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalTest {
+	
+	
+	@Test
+	public void check_sum_for_empty_string(){
+		StringCalculator strcalc = new StringCalculator();
+		Assert.assertEquals("0",strcalc.checksum(""));
+	}
+
+}
---
+++ src/test/java/tdd/xtr/StringCalculator.java
@@ -0,0 +1,9 @@
+package tdd.xtr;
+
+public class StringCalculator {
+
+	public Object checksum(String string) {
+		return string;
+	}
+
+}
