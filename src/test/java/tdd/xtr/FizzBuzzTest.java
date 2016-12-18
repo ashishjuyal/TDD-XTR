1158ba65828f3076537328ede8140dd5611c3b3b
Ashish Juyal <ajuyal@AJ-MacBookPro.local>
2016-12-18 19:31:00 +0530

    refactoring

--- src/test/java/tdd/xtr/FizzBuzzTest.java
+++ src/test/java/tdd/xtr/FizzBuzzTest.java
@@ -59,15 +59,18 @@ public class FizzBuzzTest {
         public static final String FIZZ = "Fizz";
         public static final String BUZZ = "Buzz";
         public static final String FIZZ_BUZZ = "FizzBuzz";
+        public static final int FACTOR_OF_FIZZ_BUZZ = 15;
+        public static final int FACTOR_OF_FIZZ = 3;
+        public static final int FACTOR_OF_BUZZ = 5;
         private int number;
 
         public String evaluate(int number) {
             this.number = number;
-            if(isDivisibleBy(15)) {
+            if(isDivisibleBy(FACTOR_OF_FIZZ_BUZZ)) {
                 return FIZZ_BUZZ;
-            } else if(isDivisibleBy(3)) {
+            } else if(isDivisibleBy(FACTOR_OF_FIZZ)) {
                 return FIZZ;
-            } else if(isDivisibleBy(5)) {
+            } else if(isDivisibleBy(FACTOR_OF_BUZZ)) {
                 return BUZZ;
             }
             return valueOf(number);
