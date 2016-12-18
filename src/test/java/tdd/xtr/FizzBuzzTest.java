b9e0b892d1d6e6c39f2f9109f177159e28469993
Ashish Juyal <ajuyal@AJ-MacBookPro.local>
2016-12-18 18:43:36 +0530

    return Fizz for 3 - passing test

--- src/test/java/tdd/xtr/FizzBuzzTest.java
+++ src/test/java/tdd/xtr/FizzBuzzTest.java
@@ -31,6 +31,9 @@ public class FizzBuzzTest {
 
     private class FizzBuzz {
         public String evaluate(int number) {
+            if(number == 3) {
+                return "Fizz";
+            }
             return valueOf(number);
         }
     }
