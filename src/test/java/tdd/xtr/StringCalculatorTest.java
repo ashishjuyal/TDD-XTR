aa8fd7a32808e566128bd90c424560f8a8833486
Ashish Juyal <ajuyal@AJ-MacBookPro.local>
2016-12-19 09:48:27 +0530

    String calculator - return same number when single  number - failing test

--- src/test/java/tdd/xtr/StringCalculatorTest.java
+++ src/test/java/tdd/xtr/StringCalculatorTest.java
@@ -12,6 +12,12 @@ public class StringCalculatorTest {
         assertEquals(0, stringCalculator.add(""));
     }
 
+    @Test public void
+    should_return_the_same_number_when_single_number() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(4, stringCalculator.add("4"));
+    }
+
     private class StringCalculator {
         public int add(String input) {
             return 0;
