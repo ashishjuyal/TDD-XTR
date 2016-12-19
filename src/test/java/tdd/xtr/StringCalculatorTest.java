8b5e247c1ed4391870e102ff4f27f33a3302255f
Ashish Juyal <ajuyal@AJ-MacBookPro.local>
2016-12-19 11:59:14 +0530

    String calculator - return sum of any amount of numbers - refactoring

--- src/test/java/tdd/xtr/StringCalculatorTest.java
+++ src/test/java/tdd/xtr/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package tdd.xtr;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static java.lang.Integer.valueOf;
@@ -7,29 +8,31 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
+    StringCalculator stringCalculator;
+    @Before public void
+    setup() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test public void
     should_return_zero_for_empty_string() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test public void
     should_return_the_same_number_when_single_number() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(4, stringCalculator.add("4"));
         assertEquals(5, stringCalculator.add("5"));
     }
 
     @Test public void
     should_return_the_sum_of_two_numbers() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(9, stringCalculator.add("4,5"));
         assertEquals(8, stringCalculator.add("3,5"));
     }
 
     @Test public void
     should_return_the_sum_of_any_amount_of_numbers() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(15, stringCalculator.add("1,2,3,4,5"));
     }
 
@@ -37,8 +40,6 @@ public class StringCalculatorTest {
         public int add(String input) {
             if(input.isEmpty()) {
                 return 0;
-            } else if (input.length() == 1) {
-                return valueOf(input);
             }
             String[] numbers = input.split(",");
             int sum = 0;
