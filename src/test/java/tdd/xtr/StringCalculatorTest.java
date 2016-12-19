3d41ec483c9522ec051d012e0a8bc858e202879e
Ashish Juyal <ajuyal@AJ-MacBookPro.local>
2016-12-19 12:04:56 +0530

    String calculator - throw exception when negative number - passing test

--- src/test/java/tdd/xtr/StringCalculatorTest.java
+++ src/test/java/tdd/xtr/StringCalculatorTest.java
@@ -15,45 +15,55 @@ public class StringCalculatorTest {
     }
 
     @Test public void
-    should_return_zero_for_empty_string() {
+    should_return_zero_for_empty_string() throws Exception {
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test public void
-    should_return_the_same_number_when_single_number() {
+    should_return_the_same_number_when_single_number() throws Exception {
         assertEquals(4, stringCalculator.add("4"));
         assertEquals(5, stringCalculator.add("5"));
     }
 
     @Test public void
-    should_return_the_sum_of_two_numbers() {
+    should_return_the_sum_of_two_numbers() throws Exception {
         assertEquals(9, stringCalculator.add("4,5"));
         assertEquals(8, stringCalculator.add("3,5"));
     }
 
     @Test public void
-    should_return_the_sum_of_any_amount_of_numbers() {
+    should_return_the_sum_of_any_amount_of_numbers() throws Exception {
         assertEquals(15, stringCalculator.add("1,2,3,4,5"));
     }
 
     @Test public void
-    should_return_the_sum_of_numbers_delimited_by_newline() {
+    should_return_the_sum_of_numbers_delimited_by_newline() throws Exception {
         assertEquals(15, stringCalculator.add("1\n2,3\n4,5"));
     }
 
     @Test(expected = Exception.class) public void
-    should_throw_exception_when_negative_number() {
+    should_throw_exception_when_negative_number() throws Exception {
         stringCalculator.add("1\n-2,3");
     }
 
     private class StringCalculator {
-        public int add(String input) {
+        public int add(String input) throws Exception {
             if(input.isEmpty()) {
                 return 0;
             }
             String[] numbers = extractNumbers(input);
 
+            findNegativeNumbers(numbers);
+
             return getSum(numbers);
+        }
+
+        private void findNegativeNumbers(String[] numbers) throws Exception {
+            for(String number: numbers) {
+                if(valueOf(number) < 0) {
+                    throw new Exception();
+                }
+            }
         }
 
         private int getSum(String[] numbers) {
